ce4dbd3b248345b6b2954a8b6d51009484b5e8e1
Estíbaliz Martín <estibalizmares@gmail.com>
2023-10-05 18:50:21 +0200

    Test

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_forms),
-    path('new/', views.create_form, name='create_form'),
-    path('delete_form/<int:id_formulario>/', views.delete_form, name='delete_form')
+    path('forms/', views.list_forms),
+    path('forms/new/', views.create_form, name='create_form'),
+    path('forms/delete/<int:id_formulario>/', views.delete_form, name='delete_form'),
+    path('films/', views.list_films)
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ObjectDoesNotExist
-from .models import Formulario, Usuario
+from .models import Formulario, Usuario, Pelicula
+
+pagina_principal = "/forms"
+errores = { "campos_requeridos": "Email, asunto y descripción son campos requeridos.", 
+            "form_not_found": "Formulario inexistente."}
 
 def list_forms(request):
     formularios = Formulario.objects.all()
     return render(request, 'list_forms.html', { "formularios": formularios })
 
 def create_form(request):
+
     Usuario(nombre=None, apellidos=None, email=request.POST["email"]).save()
     new_email = Usuario.objects.get(email=request.POST["email"])
     new_asunto = request.POST["asunto"]
@@ -15,16 +20,23 @@ def create_form(request):
     if new_email == "" or new_asunto == "" or new_descripcion == "":
         formularios = Formulario.objects.all()
         return render(
-            request, "list_forms.html", {"formularios": formularios, "error": "Email, asunto y descripción son requeridos."}
+            request, "list_forms.html", { "formularios": formularios, "error": errores['campos_requeridos'] }
         )
     formulario = Formulario(email=new_email, asunto=new_asunto, descripcion=new_descripcion)
     formulario.save()
-    return redirect("/forms/")
+    return redirect(pagina_principal)
 
 def delete_form(request, id_formulario):
     try:
         formulario = Formulario.objects.get(id_formulario = id_formulario)
         formulario.delete()
     except ObjectDoesNotExist:
-        print("El usuario no existe.")
-    return redirect("/forms/")
+        formularios = Formulario.objects.all()
+        return render(
+            request, "list_forms.html", { "formularios": formularios, "error": errores['form_not_found']}
+        )
+    return redirect(pagina_principal)
+
+def list_films(request):
+    peliculas = Pelicula.objects.all()
+    return render(request, 'list_films.html', { "peliculas": peliculas })
