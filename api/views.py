2fbb4a5f982fa34431d3349724c99cbe3ac0ad81
Estíbaliz Martín <estibalizmares@gmail.com>
2023-10-05 19:07:57 +0200

    Verify if email exists in database

--- api/views.py
+++ api/views.py
@@ -8,21 +8,22 @@ errores = { "campos_requeridos": "Email, asunto y descripción son campos requer
 
 def list_forms(request):
     formularios = Formulario.objects.all()
+    print(formularios)
     return render(request, 'list_forms.html', { "formularios": formularios })
 
 def create_form(request):
-
-    Usuario(nombre=None, apellidos=None, email=request.POST["email"]).save()
-    new_email = Usuario.objects.get(email=request.POST["email"])
+    usuario = Usuario.objects.get(email=request.POST["email"])
+    if usuario is None:
+        Usuario(nombre=None, apellidos=None, email=request.POST["email"]).save()
     new_asunto = request.POST["asunto"]
     new_descripcion = request.POST["descripcion"]
 
-    if new_email == "" or new_asunto == "" or new_descripcion == "":
+    if usuario == "" or new_asunto == "" or new_descripcion == "":
         formularios = Formulario.objects.all()
         return render(
             request, "list_forms.html", { "formularios": formularios, "error": errores['campos_requeridos'] }
         )
-    formulario = Formulario(email=new_email, asunto=new_asunto, descripcion=new_descripcion)
+    formulario = Formulario(email=usuario, asunto=new_asunto, descripcion=new_descripcion)
     formulario.save()
     return redirect(pagina_principal)
 
